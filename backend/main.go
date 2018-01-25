78b70b2551828bde3c15f5d04f6b75165240a9c9
William Price <bobkidbob@bobkidbob.com>
2018-01-25 15:06:09 +0000

    Implemented all HTTP methods

--- backend/main.go
+++ backend/main.go
@@ -10,13 +10,19 @@ import (
 
 var dev bool
 var pages struct {
+    MethodNotAllowed []byte
     NotFound []byte
     NotImplemented []byte
 }
 
 func init() {
     dev = appengine.IsDevAppServer()
-    f, err := ioutil.ReadFile("frontend/not-found.html")
+    f, err := ioutil.ReadFile("frontend/method-not-allowed.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+    pages.MethodNotAllowed = f
+    f, err = ioutil.ReadFile("frontend/not-found.html")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -55,11 +61,25 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func serve(w http.ResponseWriter, r *http.Request, url *url.URL) {
     switch r.Method {
-    case http.MethodHead:
-        head(w, r, url)
     case http.MethodGet:
         head(w, r, url)
-        get(w, r, url)
+        get(w, url)
+    case http.MethodHead:
+        head(w, r, url)
+    case http.MethodPost:
+        methodNotAllowed(w)
+    case http.MethodPut:
+        methodNotAllowed(w)
+    case http.MethodPatch:
+        methodNotAllowed(w)
+    case http.MethodDelete:
+        methodNotAllowed(w)
+    case http.MethodConnect:
+        methodNotAllowed(w)
+    case http.MethodOptions:
+        methodNotAllowed(w)
+    case http.MethodTrace:
+        methodNotAllowed(w)
     default:
         w.WriteHeader(http.StatusNotImplemented)
         w.Write(pages.NotImplemented)
@@ -75,13 +95,18 @@ func head(w http.ResponseWriter, r *http.Request, url *url.URL) {
     }
 }
 
-func get(w http.ResponseWriter, r *http.Request, url *url.URL) {
+func get(w http.ResponseWriter, url *url.URL) {
     switch url.Path {
     case "/":
         return
     default:
         w.Write(pages.NotFound)
     }
+}
+
+func methodNotAllowed(w http.ResponseWriter) {
+    w.WriteHeader(http.StatusMethodNotAllowed)
+    w.Write(pages.MethodNotAllowed)
 }
 
 func pathAndQuery(url *url.URL) string {
